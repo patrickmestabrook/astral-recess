73115fb5464bd68cf7724612a1e466f9c4ed079c
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-08-25 10:40:15 -0600

    remove some old, unused files

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,3 @@
-export {default as MeditationPlaylist} from './MeditationPlaylist';
-export {default as NoiseGenerator} from './NoiseGenerator';
-export {default as ToneGenerators} from './ToneGenerators';
 export {default as Button} from './Button';
 export {default as Layout} from './Layout';
 export {default as VerticalRange} from './VerticalRange';
