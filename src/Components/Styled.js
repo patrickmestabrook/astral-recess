c63018ba12052e3f1147ebe404c070df989d532a
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-08-29 11:58:56 -0600

    Create some simple styled components to cleanup Layout

---
+++ src/Components/Styled.js
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const ControlsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+  padding: 20px;
+`;
+
+export const ControlGroup = styled.div`
+  margin-right: 100px;
+`;
+
+export const ControlLabel = styled.label`
+  display: block;
+  text-align: center;
+`;
+
+export const Controls = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
