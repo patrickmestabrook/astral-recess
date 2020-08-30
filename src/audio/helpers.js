65cadca6339272d519c0f9f8bac50749068bbfd7
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-08-30 14:10:22 -0600

    Add createPinkNoiseNode(), createPinkNoiseOscillator(), and setAllAudioParameters()

--- src/audio/helpers.js
+++ src/audio/helpers.js
@@ -1,6 +1,13 @@
+/**
+ * @file Provides functions that interface directly with the web audio API
+ * @author Patrick Estabrook
+ */
 
-/*
- * createAudioContext()
+
+/**
+ * @function
+ * @name createAudioContext
+ * @returns {void} Nothing
  */
 export const createAudioContext = () => {
   const AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -18,23 +25,54 @@ export const createAudioContext = () => {
   return [audioContext, masterGain, masterAnalyser];
 }
 
-/*
- * setMasterVolume()
+
+/**
+ * @function
+ * @name setMasterVolume
+ * @param {AudioGainNode} masterGain - the node representing the master gain level
+ * @param {BaseAudioContext} audioContext - global audio context object
+ * @param {Number} action.payload - the gain value to set master gain to, between 0 and 1
+ * @returns {void} Nothing
  */
-export const setMasterVolume = (masterVolume, audioContext, { payload }) => {
-  masterVolume.gain.linearRampToValueAtTime(payload, audioContext.currentTime + 0.1);
+export const setMasterVolume = (masterGain, audioContext, { payload }) => {
+  masterGain.gain.linearRampToValueAtTime(payload, audioContext.currentTime + 0.1);
 };
 
-/*
- * setOscillatorParameter()
+
+/**
+ * @function
+ * @name setAudioNodeParameter
+ * @param {[AudioNode]} audioNodes - an array of AudioNodes whose parameters we can set
+ * @param {Object} action.payload - an object specifying the AudioNode index, parameter name, and target value
+ * @returns {void} Nothing
  */
-export const setOscillatorParameter = (oscillators, { payload }) => {
-  const { oscillatorIndex, parameter, value } = payload;
-  oscillators[oscillatorIndex][parameter].value = value;
+export const setAudioNodeParameter = (audioNodes, { payload }) => {
+  const { audioNodeIndex, parameter, value } = payload;
+  audioNodes[audioNodeIndex][parameter].value = value;
 };
 
-/*
- * createOscillator()
+export const setAllAudioParameters= (audioContext, masterGain, audioNodes, { payload }) => {
+  const { mixer, audioNodes: newAudioNodes } = payload;
+  masterGain.gain.linearRampToValueAtTime(mixer.masterVolume, audioContext.currentTime + 4);
+  audioNodes.forEach((audioNode, i) => {
+    if (newAudioNodes[i].frequency) {
+      audioNode.frequency.linearRampToValueAtTime(newAudioNodes[i].frequency, audioContext.currentTime + 4);
+    }
+    if (newAudioNodes[i].detune) {
+      audioNode.detune.linearRampToValueAtTime(newAudioNodes[i].detune, audioContext.currentTime + 4);
+    }
+    if (newAudioNodes[i].gain) {
+      audioNode.gain.linearRampToValueAtTime(newAudioNodes[i].gain, audioContext.currentTime + 4);
+    }
+    if (newAudioNodes[i].Q) {
+      audioNode.Q.linearRampToValueAtTime(newAudioNodes[i].Q, audioContext.currentTime + 4);
+    }
+  });
+};
+
+/**
+ * @function
+ * @name createOscillator
  */
 export const createOscillator = (
   audioContext,
@@ -45,7 +83,7 @@ export const createOscillator = (
     detune = 0,
     gain = 0.5,
     pan = 0,
-  }
+  } = {},
 ) => {
   if (!audioContext || !audioContext.createOscillator) {
     throw new Error('Please provide an AudioContext.');
@@ -68,5 +106,65 @@ export const createOscillator = (
   oscillator.start();
   stereoPanner.connect(destinationNode);
 
-  return [oscillator, gain, analyser, stereoPanner];
+  return [oscillator, gainNode, analyser, stereoPanner];
+};
+
+
+/**
+ * @function
+ * @name createPinkNoiseNode
+ * @param {BaseAudioContext} audioContext - the global web  audio context
+ * @returns {AudioNode} the audio node representing the pink noise oscillator
+ * @TODO audioContext.createScriptProcessor() is a bad pattern
+ * opt for AudioWorklets once we can figure out how to get them working
+ */
+const createPinkNoiseNode = (audioContext) => {
+  const bufferSize = 4096;
+  var b0, b1, b2, b3, b4, b5, b6;
+      b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+  const node = audioContext.createScriptProcessor(bufferSize, 1, 1);
+  node.onaudioprocess = function(e) {
+      const output = e.outputBuffer.getChannelData(0);
+      for (let i = 0; i < bufferSize; i++) {
+          const white = Math.random() * 2 - 1;
+          b0 = 0.99886 * b0 + white * 0.0555179;
+          b1 = 0.99332 * b1 + white * 0.0750759;
+          b2 = 0.96900 * b2 + white * 0.1538520;
+          b3 = 0.86650 * b3 + white * 0.3104856;
+          b4 = 0.55000 * b4 + white * 0.5329522;
+          b5 = -0.7616 * b5 - white * 0.0168980;
+          output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+          output[i] *= 0.11; // (roughly) compensate for gain
+          b6 = white * 0.115926;
+      }
+  }
+  return node;
+};
+
+
+/**
+ * @function
+ * @name createPinkNoiseOscillator
+ */
+export const createPinkNoiseOscillator = (
+  audioContext,
+  destinationNode,
+  {
+    filterType = 'lowpass',
+    filterFrequency = 5000,
+    filterQ = 0,
+    gain = 0.01,
+  } = {},
+) => {
+  const pinkNoise = createPinkNoiseNode(audioContext);
+  const biquadFilter = audioContext.createBiquadFilter();
+  const gainNode =  audioContext.createGain();
+  biquadFilter.type = filterType;
+  biquadFilter.frequency.value = filterFrequency;
+  biquadFilter.Q.value = filterQ;
+  gainNode.gain.value = gain;
+  pinkNoise.connect(biquadFilter);
+  biquadFilter.connect(gainNode);
+  gainNode.connect(destinationNode);
+  return [biquadFilter, gainNode];
 };
