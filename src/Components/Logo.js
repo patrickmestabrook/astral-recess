df4f7c08d39371b69882b7cc371574fb8991fbcc
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-12-02 13:01:52 -0500

    Logo refinements

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,22 +3,23 @@ import styled from '@emotion/styled';
 
 
 const LogoWrapper = styled.div`
-  font-family: 'Comfortaa', cursive;
+  font-family: 'Roboto Slab', serif;
   color: black;
-  font-size: 3rem;
-  width: 270px;
-  height: 270px;
+  font-size: 2.1rem;
+  width: 200px;
+  height: 200px;
   display: flex;
   align-items: center;
   justify-content: center;
-  /* background: rgba(0,0,0,0.1); */
-  box-shadow: 0 0 0 25px rgba(0,0,0,1);
+  background: rgba(255,255,255,0);
+  box-shadow: 0 0 0 20px rgba(0,0,0,1);
   margin-right: 160px;
+  /* text-shadow:  4px 3px 0px rgba(255,255,255,0.3); */
 `;
 function Logo() {
   return (
     <LogoWrapper>
-      Astral <br /> Recess
+      astral <br /> recess
     </LogoWrapper>
   );
 }
